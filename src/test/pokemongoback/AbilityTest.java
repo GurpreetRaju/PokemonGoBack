acd53a9437749d1ac5f3f5f29a0127b737f7a693
gagankhalsa <gagankhalsa@gmail.com>
2017-06-06 13:01:09 -0400

    test case
    checking it

---
+++ src/test/pokemongoback/AbilityTest.java
@@ -0,0 +1,14 @@
+package test.pokemongoback;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AbilityTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
