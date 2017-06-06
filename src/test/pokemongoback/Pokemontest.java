c0e78057d32ee261f6464d39fd9e488c99a02235
gagankhalsa <gagankhalsa@gmail.com>
2017-06-06 12:55:27 -0400

    test
    jgkghk

---
+++ src/test/pokemongoback/Pokemontest.java
@@ -0,0 +1,14 @@
+package test.pokemongoback;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class Pokemontest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
